f03d460fd91483d3032c359c024b0bbfd86d94ce
rizkiwnf <rizkiwahyunf2@gmail.com>
2022-03-24 01:39:57 +0700

    Menambahkan LayoutStateManagementPart2

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,66 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:layoutminggu4_2/detail_screen.dart';
-import 'package:layoutminggu4_2/model/tourism_place.dart';
+import 'package:statemanagementp2/detail_screen.dart';
+import 'package:statemanagementp2/model/tourism_place.dart';
+import 'package:statemanagementp2/tourism_list.dart';
+import 'done_tourism_list.dart';
 
-class MainScreen extends StatelessWidget {
-  const MainScreen({Key? key}) : super(key: key);
+class MainScreen extends StatefulWidget {
+  MainScreen({Key? key}) : super(key: key);
 
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
 
+class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Wisata Bandung'),
-      ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final TourismPlace place = tourismPlaceList[index];
-          return InkWell(
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context){
-                return DetailScreen(place: place);
-              }));
+        title: const Text('Wisata Surabaya'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.done_outline),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  return const DoneTourismList();
+                }),
+              );
             },
-            child: listItem(place),
-          );
-        },
-        itemCount: tourismPlaceList.length,
-      ),
-    );
-  }
-
-  Widget listItem(place){
-    return Card(
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Expanded(
-            flex: 1,
-            child: Image.asset(place.imageAsset),
-          ),
-          Expanded(
-            flex: 2,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget> [
-                  Text(
-                    place.name,
-                    style: const TextStyle(fontSize: 16.0),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(place.location),
-                ],
-              ),
-            ),
-          ),
+          )
         ],
       ),
+      body: TourismList(),
     );
   }
 }
